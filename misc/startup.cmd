714dd1fbd66f520dafb73403b48e7c26a37a7942
Imran Hameed <idhameed@gmail.com>
2020-09-07 14:26:51 -0700

    -

--- misc/startup.cmd
+++ misc/startup.cmd
@@ -2,7 +2,8 @@ setx HOME "%USERPROFILE%"
 setx DISPLAY ":0"
 
 set FN="%TEMP%\ssh-agent-init.cmd"
-C:\cygwin\bin\ssh-pageant.exe -c | C:\cygwin\bin\sed.exe 's/setenv/setx/' | C:\cygwin\bin\sed "s/'//g" | C:\cygwin\bin\sed 's/;$//' > %FN%
+C:\cygwin\bin\mkdir -p /tmp/ssh-__pageant
+C:\cygwin\bin\ssh-pageant.exe -c -r -a /tmp/ssh-__pageant/agent.0 | C:\cygwin\bin\sed.exe 's/setenv/setx/' | C:\cygwin\bin\sed "s/'//g" | C:\cygwin\bin\sed 's/;$//' > %FN%
 call %FN%
 del %FN%
 
